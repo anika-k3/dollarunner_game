78fc3e3eee98b5b9eff4dfd5753d1eb244087608
anika-k3 <117938047+anika-k3@users.noreply.github.com>
2022-12-13 14:46:14 -0500

    Created high score scenes, added some documentation

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Created a class for movement for the character
 public class movement : MonoBehaviour
 {
+    // Creates a float for the horizontal movement
     private float horizontal;
+    // Creates a float set to the speed of the character's movements
     private float speed = 8f;
+    // Creates a float set to the jumping power of the character
     private float jumpingPower = 24f;
+    // 
     private bool isFacingRight = true;
 
     [SerializeField] private Rigidbody2D rb;
@@ -17,7 +22,6 @@ public class movement : MonoBehaviour
     [SerializeField] private int sceneID2;
     [SerializeField] private int highscore;
 
-    // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
@@ -44,8 +48,10 @@ public class movement : MonoBehaviour
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
     }
 
+    // Used to flip the player's direction in the x-axis
     private void Flip()
     {
+        // 
         if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
             isFacingRight = !isFacingRight;
@@ -54,24 +60,30 @@ public class movement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+    // This code activates when there is a 2D trigger collision that happens between objects
     private void OnTriggerEnter2D(Collider2D collider)
     {
+        // If the player collides with a tag called NextLevel, then the scene will change to be the next level
         if (collider.gameObject.CompareTag("NextLevel"))
         {
             Debug.Log("Trigger");
             SceneManager.LoadScene(sceneID);
         }
     }
+    // This code activates when there is a 2D Collision within objects
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        // If the player collides with a tag called Money, then the money disappears
         if (collision.gameObject.CompareTag("Money"))
         {
             Debug.Log("Collision");
             Destroy(collision.gameObject);
             highscore += 1;
         }
+        // Sends the highscore to the console
         Debug.Log(highscore);
 
+        // If the player collides with an object called TriggersDeath, then the scene will change to the death scene
         if (collision.gameObject.CompareTag("TriggersDeath"))
         {
             Debug.Log("Collision");
