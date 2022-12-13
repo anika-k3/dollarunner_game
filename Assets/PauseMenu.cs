9434e9e17bd6849cf7ec9f11ee4dfef81d045870
anika-k3 <117938047+anika-k3@users.noreply.github.com>
2022-12-13 11:00:34 -0500

    Created a pause menu that is now working in levels 2 and 3
    Added some documentation, need to add more

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Creates a class called PauseMenu, which will be used to pause the game
 public class PauseMenu : MonoBehaviour
 {
+    // Creates a game object called pauseMenu
     [SerializeField] GameObject pauseMenu;
 
-    public void Pause(int sceneID)
+    // Will play when the option "Pause" is selected from PauseMenu, uses an integer called sceneID which is allocated to a scene number in the build
+    public void Pause()
     {
-        Time.timeScale = 0;
+        // sets the pause menu as active when clicked, so that the hidden panel will show up
+        pauseMenu.SetActive(true);
+        // Everything in the background stops as time gets set to zero
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    
+    public void Home(int sceneID)
+    {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(sceneID);
-        Time.timeScale = 0;
     }
 }
--- Assets/ResumeMenu.cs
+++ Assets/ResumeMenu.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Creates a class called ResumeMenu, which will be used to resume the game
 public class ResumeMenu : MonoBehaviour
 {
+    // Creates a game object called "resumeMenu"
     [SerializeField] GameObject resumeMenu;
 
+    // Will play when the option "Resume" is selected from the ResumeMenu, uses an integer called sceneID which is allocated to a scene number in the build
     public void Resume(int sceneID)
     {
+        // Loads the scene selected 
         SceneManager.LoadScene(sceneID);
+        //
         Time.timeScale = 1;
     }
 }
